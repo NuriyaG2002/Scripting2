84a998e05b84e5ac5b426197eb28287126cc713c
NuriyaG2002 <155141187+NuriyaG2002@users.noreply.github.com>
2023-12-30 16:06:50 +0500

    Add files via upload

---
+++ HeiioWorld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeiioWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //приветствее миру
+        print("Heiio, World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
